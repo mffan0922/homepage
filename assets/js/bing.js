a77c1a435d4c69480f6a9ad7d11c766e49d18b84
mffan0922 <mffan0922@163.com>
2022-04-13 22:14:00 +0800

    [modify]: bing image auto pick

--- assets/js/bing.js
+++ assets/js/bing.js
@@ -4,7 +4,7 @@ const fs = require('fs')
 const options = {
   hostname: 'www.bing.com',
   port: 443,
-  path: '/HPImageArchive.aspx?format=js&idx=-1&n=8',
+  path: '/HPImageArchive.aspx?format=js&idx=0&n=8',
   method: 'GET'
 }
 
